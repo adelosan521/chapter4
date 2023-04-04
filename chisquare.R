0622a147d842e59ccd8f33f8c9b3123ca9b96b2b
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-04 12:59:46 +0100

    Create chisquare.R

---
+++ chisquare.R
@@ -0,0 +1,21 @@
+## Chi-square test for comparing TF footprint frequency at VGCC loci versus random genes
+# Define the observed counts
+observed_counts <- matrix(c(5, 7, 27, 21), nrow = 2)
+
+# Define the row and column names
+rownames(observed_counts) <- c("VGCC loci", "Random genes")
+colnames(observed_counts) <- c("TF motif present", "TF motif absent")
+
+# Perform the chi-square test
+result <- chisq.test(observed_counts)
+
+# Print the chi-square statistic and p-value
+cat("Chi-squared statistic: ", result$statistic, "\n")
+cat("p-value: ", result$p.value, "\n")
+
+# Check for significance at 0.05 level (i.e. alpha is set at 0.05)
+if(result$p.value < 0.05){
+  cat("The difference in TF motif frequency between VGCC loci and random genes is significant.\n")
+}else{
+  cat("There is no significant difference in TF motif frequency between VGCC loci and random genes.\n")
+}
