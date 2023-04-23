0baf89bf8f0d535b37b07908e88d56804811d0de
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-23 23:19:23 +0100

    Update naive_formative.sh

--- naive_formative.sh
+++ naive_formative.sh
@@ -13,17 +13,12 @@ module load python-cbrg
 TOBIAS --version
 
 ##ATACorrect (correction of Tn5 bias) of d0 and d1 samples
-
 TOBIAS ATACorrect --bam d0_sorted.bam --peaks merged_peaks_d0vsd1.bed --genome GRCm39.primary_assembly.genome.fa --blacklist mm10-blacklist.v2.bed --outdir d0_ATACorrect --cores 8
-
 TOBIAS ATACorrect --bam d1_sorted.bam --peaks merged_peaks_d0vsd1.bed --genome GRCm39.primary_assembly.genome.fa --blacklist mm10-blacklist.v2.bed --outdir d1_ATACorrect --cores 8
 
 ##Footprints of d0 and d1 samples to calculate footprinting scores 
-
 TOBIAS FootprintScores --signal /project/tunbridgelab/aangeles/atacseq/validation/naive-formative/d0_ATACorrect/d0_sorted_corrected.bw --regions merged_peaks_d0vsd1.bed --output d0vsd1_d1_footprints.bw --cores 8
-
 TOBIAS FootprintScores --signal /project/tunbridgelab/aangeles/atacseq/validation/naive-formative/d1_ATACorrect/d1_sorted_corrected.bw --regions merged_peaks_d0vsd1.bed --output d0vsd1_d1_footprints.bw --cores 8
 
 ##Compare d0 and d1 samples using BINDetect function (identify bound/unbound sites, compare footprints from different conditions)
-
 TOBIAS BINDetect --motifs JASPAR2022_CORE_non-redundant_pfms_jaspar.txt --signals d0vsd1_d0_footprints.bw d0vsd1_d1_footprints.bw --genome GRCm39.primary_assembly.genome.fa --peaks merged_peaks_d0vsd1-clean.bed --outdir d0_d1_comparison --cond_names d0_naive d1_formative
